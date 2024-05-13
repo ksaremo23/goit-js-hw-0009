04631495b41562113076327960675d7a9e8e2f76
ksaremo23 <120180371+ksaremo23@users.noreply.github.com>
2024-05-13 06:10:25 +0000

    Deploying to gh-pages from @ ksaremo23/goit-js-hw-0009@3082d77a5643200ce9ff27231070ac66f3bc613a 🚀

--- 01-color-switcher.07dff997.js
+++
@@ -1,2 +0,0 @@
-const t={btnStartE1:document.querySelector("button[data-start]"),btnStopE1:document.querySelector("button[data-stop]"),bodyE1:document.body},{start:e,stop:o}=createChangerColor(t);t.btnStartE1.addEventListener("click",e),t.addEventListener("click",o);
-//# sourceMappingURL=01-color-switcher.07dff997.js.map
