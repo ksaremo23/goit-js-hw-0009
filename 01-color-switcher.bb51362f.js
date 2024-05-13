f188f57873cc41d893d3eaa82ad36b62ecdfb70c
ksaremo23 <120180371+ksaremo23@users.noreply.github.com>
2024-05-13 01:08:22 +0000

    Deploying to gh-pages from @ ksaremo23/goit-js-hw-0009@d08411a9b9353e3903aa77243310dafcee5212fd 🚀

---
+++ 01-color-switcher.bb51362f.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStartE1:document.querySelector("button[data-start]"),btnStopE1:document.querySelector("button[data-stop]"),bodyE1:document.body},e=createChangerColor(t),n=e.start,o=e.stop;t.btnStartE1.addEventListener("click",n),t.addEventListener("click",o)}();
+//# sourceMappingURL=01-color-switcher.bb51362f.js.map
